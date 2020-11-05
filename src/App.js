6ba1559af7ef45150c4a81b5c8f6e04ed1b76a9e
Arif Bashar <arifbasharx@gmail.com>
2020-11-05 09:18:32 -0600

    basic layout for file upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+import styled from 'styled-components';
 
 function App() {
+
+  const uploadHandler = () => {
+    // fire upload events
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <FormContainer method="post">
+        <Row>
+          <Col sm={2}><NumberContainer>1</NumberContainer></Col>
+          <Col sm={10}><FormTitle>Upload your files</FormTitle>
+          <FormSubtitle>Files should be in .xlsx format</FormSubtitle></Col>
+        </Row>
+        <Row>
+          <Form.Group><Form.File id="gta-file" label="List of GTAs"></Form.File></Form.Group>
+        </Row>
+        <Row>
+        <Form.Group><Form.File id="gta-file" label="List of courses"></Form.File></Form.Group>
+        </Row>
+        
+        <Button variant="primary" type="submit" onClick={uploadHandler}>
+          Upload
+        </Button>
+      </FormContainer>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+display: flex;
+width: 100vw;
+height: 100vh;
+overflow: none;
+background: #333B51;
+justify-content: center;
+align-items: center;
+`;
+
+const FormContainer = styled.form`
+  min-width: 0%;
+  display: flex;
+  flex-direction: column;
+  background: #FFFFFF;
+  border-radius: 15px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding-left: 65px;
+  padding-right: 65px;
+  padding-bottom: 70px;
+  padding-top: 70px;
+`;
+
+const NumberContainer = styled.div`
+display: flex;
+width: 40px;
+height: 40px;
+border-radius: 50%;
+background: #A9BCFF;
+justify-content: center;
+align-items: center;
+
+color: white;
+font-weight: 700;
+`;
+
+const FormTitle = styled.h4`
+color: #585858;
+font-weight: 700;
+ `;
+const FormSubtitle = styled.h5`
+color: #7B7B7B;
+font-weight: 300;
+ `;
+
+
 
 export default App;
