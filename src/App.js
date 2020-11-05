56e03fa68a040a931c74fafde7c6a0683d51a7c3
Arif Bashar <arifbasharx@gmail.com>
2020-11-05 09:24:05 -0600

    formatting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import Container from "react-bootstrap/Button";
+import React from "react";
+// import Container from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import './App.css';
-import styled from 'styled-components';
+import styled from "styled-components";
 
 function App() {
-
   const uploadHandler = () => {
     // fire upload events
-  }
+  };
+
   return (
     <AppContainer>
       <FormContainer method="post">
-        <Row>
-          <Col sm={2}><NumberContainer>1</NumberContainer></Col>
-          <Col sm={10}><FormTitle>Upload your files</FormTitle>
-          <FormSubtitle>Files should be in .xlsx format</FormSubtitle></Col>
+        <Row style={{ marginBottom: 70 }}>
+          <Col sm={2}>
+            <NumberContainer>1</NumberContainer>
+          </Col>
+          <Col sm={10}>
+            <FormTitle>Upload your files</FormTitle>
+            <FormSubtitle>Files should be in .xlsx format</FormSubtitle>
+          </Col>
         </Row>
-        <Row>
-          <Form.Group><Form.File id="gta-file" label="List of GTAs"></Form.File></Form.Group>
+        <Row style={{ marginBottom: 60 }}>
+          <Form.Group>
+            <Form.File id="gta-file" label="List of GTAs"></Form.File>
+          </Form.Group>
         </Row>
-        <Row>
-        <Form.Group><Form.File id="gta-file" label="List of courses"></Form.File></Form.Group>
+        <Row style={{ marginBottom: 70 }}>
+          <Form.Group>
+            <Form.File id="gta-file" label="List of courses"></Form.File>
+          </Form.Group>
         </Row>
-        
+
         <Button variant="primary" type="submit" onClick={uploadHandler}>
           Upload
         </Button>
@@ -37,20 +45,20 @@ function App() {
 }
 
 const AppContainer = styled.div`
-display: flex;
-width: 100vw;
-height: 100vh;
-overflow: none;
-background: #333B51;
-justify-content: center;
-align-items: center;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  overflow-x: none;
+  background: #333b51;
+  justify-content: center;
+  align-items: center;
 `;
 
 const FormContainer = styled.form`
   min-width: 0%;
   display: flex;
   flex-direction: column;
-  background: #FFFFFF;
+  background: #ffffff;
   border-radius: 15px;
   margin-left: 20px;
   margin-right: 20px;
@@ -61,27 +69,26 @@ const FormContainer = styled.form`
 `;
 
 const NumberContainer = styled.div`
-display: flex;
-width: 40px;
-height: 40px;
-border-radius: 50%;
-background: #A9BCFF;
-justify-content: center;
-align-items: center;
+  display: flex;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #a9bcff;
+  justify-content: center;
+  align-items: center;
 
-color: white;
-font-weight: 700;
+  color: white;
+  font-weight: 700;
 `;
 
 const FormTitle = styled.h4`
-color: #585858;
-font-weight: 700;
- `;
+  color: #585858;
+  font-weight: 700;
+`;
+
 const FormSubtitle = styled.h5`
-color: #7B7B7B;
-font-weight: 300;
- `;
-
-
+  color: #7b7b7b;
+  font-weight: 300;
+`;
 
 export default App;
