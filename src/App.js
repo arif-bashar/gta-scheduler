22426fc3885cbd8f884e3df41327dd7fb3f79ac5
Arif Bashar <arifbasharx@gmail.com>
2020-11-23 16:45:20 -0600

    cleaned up ui

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import Nav from "react-bootstrap/Nav";
+import Tab from "react-bootstrap/Tab";
 
 // import './App.css';
 
@@ -33,9 +35,8 @@ function App() {
   useEffect(() => {
     if (gtasBSTR != null && coursesBSTR != null) {
       setSchedule(getSimpleLabs(gtasBSTR, coursesBSTR));
-
     }
-  }, [gtasBSTR, coursesBSTR])
+  }, [gtasBSTR, coursesBSTR]);
 
   // Update gtasFile state every time file gets changed
   const gtasFileHandler = (event) => {
@@ -50,12 +51,12 @@ function App() {
   // Update gtasBSTR state
   const gtasReadHandler = (e) => {
     setGTAsBSTR(e.target.result);
-  }
+  };
 
   // Update coursesBSTR state
   const coursesReadHandler = (e) => {
     setCoursesBSTR(e.target.result);
-  }
+  };
 
   // Handler for upload button
   const uploadHandler = () => {
@@ -72,22 +73,32 @@ function App() {
   };
 
   const lab = function (item) {
-
     console.log(item);
     let students = new Set();
     for (let stud of item) {
       students.add({
         key: stud.Student,
-        classes: []
+        classes: [],
       });
     }
     sched.map((item, index) => {
       item.labs.map((thing, position) => {
-
         let temp = (
-          <Card key={position} style={{ marginBottom: "1rem" }}>
+          <Card
+            key={position}
+            style={{
+              paddingTop: 20,
+              paddingBottom: 20,
+              paddingLeft: 20,
+              paddingRight: 20,
+              marginRight: 10,
+              marginBottom: "1rem",
+            }}
+          >
             <Card.Title>{thing.Course}</Card.Title>
-            <Card.Subtitle>{thing.CRN + " " + item.Student}</Card.Subtitle>
+            <Card.Subtitle>
+              {"CRN: " + thing.CRN + "   |   Student: " + item.Student}
+            </Card.Subtitle>
             <Card.Text>
               <Table responsive bordered>
                 <thead>
@@ -101,65 +112,72 @@ function App() {
                 </thead>
                 <tbody>
                   <tr>
-                    <td>{(thing.Days.M ? thing.Begin + "-" + thing.End : "")}</td>
-                    <td>{(thing.Days.T ? thing.Begin + "-" + thing.End : "")}</td>
-                    <td>{(thing.Days.W ? thing.Begin + "-" + thing.End : "")}</td>
-                    <td>{(thing.Days.R ? thing.Begin + "-" + thing.End : "")}</td>
-                    <td>{(thing.Days.F ? thing.Begin + "-" + thing.End : "")}</td>
+                    <td>{thing.Days.M ? thing.Begin + "-" + thing.End : ""}</td>
+                    <td>{thing.Days.T ? thing.Begin + "-" + thing.End : ""}</td>
+                    <td>{thing.Days.W ? thing.Begin + "-" + thing.End : ""}</td>
+                    <td>{thing.Days.R ? thing.Begin + "-" + thing.End : ""}</td>
+                    <td>{thing.Days.F ? thing.Begin + "-" + thing.End : ""}</td>
                   </tr>
                 </tbody>
               </Table>
             </Card.Text>
           </Card>
-        )
+        );
         students.forEach((student) => {
           if (student.key === item.Student) {
             student.classes.push(temp);
           }
-
-        })
-      })
-
-    })
+        });
+      });
+    });
     return students;
-  }
+  };
 
   const dropDownItems = (schedules) => {
-
-    let items = []
+    let items = [];
     schedules.forEach((item, index) => {
       items.push(
-        <Dropdown.Item onClick={() => showSchedule(item)} key={index}>{item.key}</Dropdown.Item>
-      )
+        <Nav.Item onClick={() => showSchedule(item)} key={index}>
+          <Nav.Link eventKey={item.key}>{item.key}</Nav.Link>
+        </Nav.Item>
+      );
       console.log(item.key);
-    })
+    });
     return items;
-  }
+  };
 
   if (sched) {
     let schedules = lab(sched);
     let dropDowns = dropDownItems(schedules);
     console.log(schedules);
     return (
-      <AppContainer>
-        <Card>
-          <Card.Body>
-            <Card.Title>Schedules By GA</Card.Title>
-            <DropdownButton id="dropdown-basic-button" title="Select GA">
-              {dropDowns}
-            </DropdownButton>
-            <Card>
-              <Card.Body>
-                {(schedDisplay ? schedDisplay.classes : null)}
-              </Card.Body>
-            </Card>
-
-          </Card.Body>
-        </Card>
-      </AppContainer >
+      <ScheduleView style={{ height: schedDisplay ? "100%" : "100vh" }}>
+        <GTASide>
+          <Row>
+            <Col sm={3}>
+              <NumberContainer style={{ background: "#547aff" }}>
+                2
+              </NumberContainer>
+            </Col>
+            <Col>
+              <FormTitle style={{ color: "white" }}>Select GTA</FormTitle>
+            </Col>
+          </Row>
+          <Row>
+            <GTAContainer>
+              <Nav fill variant="pills" className="flex-column">
+                {dropDowns}
+              </Nav>
+            </GTAContainer>
+          </Row>
+        </GTASide>
+        <ScheduleSide>
+          <ScheduleContainer>
+            {schedDisplay ? schedDisplay.classes : null}
+          </ScheduleContainer>
+        </ScheduleSide>
+      </ScheduleView>
     );
-
-
   }
 
   if (!sched) {
@@ -177,18 +195,26 @@ function App() {
           </Row>
           <Row style={{ marginBottom: 60 }}>
             <Form.Group>
-              <Form.File id="gta-file" label="List of GTAs" onChange={gtasFileHandler}></Form.File>
+              <Form.File
+                id="gta-file"
+                label="List of GTAs"
+                onChange={gtasFileHandler}
+              ></Form.File>
             </Form.Group>
           </Row>
           <Row style={{ marginBottom: 70 }}>
             <Form.Group>
-              <Form.File id="gta-file" label="List of courses" onChange={coursesFileHandler}></Form.File>
+              <Form.File
+                id="gta-file"
+                label="List of courses"
+                onChange={coursesFileHandler}
+              ></Form.File>
             </Form.Group>
           </Row>
 
           <Button variant="primary" type="button" onClick={uploadHandler}>
             Upload
-    </Button>
+          </Button>
         </FormContainer>
       </AppContainer>
     );
@@ -197,50 +223,109 @@ function App() {
 
 // Styled Components
 const AppContainer = styled.div`
-display: flex;
-width: 100vw;
-height: 100%;
-overflow-x: none;
-background: #333b51;
-justify-content: center;
-align-items: center;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  overflow-x: none;
+  background: #333b51;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ScheduleView = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: none;
+  overflow-x: none;
+  background: #333b51;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GTASide = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 20vw;
+  height: 100vh;
+  overflow-y: none;
+  overflow-x: none;
+  background: #333b51;
+  justify-content: center;
+  padding-left: 60px;
+  padding-right: 60px;
+`;
+
+const GTAContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 410px;
+  width: 100%;
+  overflow-y: scroll;
+  overflow-x: none;
+  background: #414b67;
+  margin-top: 20px;
+`;
+
+const ScheduleSide = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: none;
+  overflow-x: none;
+  background: #333b51;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ScheduleContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  width: 90%;
+  height: 90%;
+  overflow-y: scroll;
+  overflow-x: none;
+  background: #98afff;
+  border-radius: 10px;
+  justify-content: space-between;
+  padding: 20px 20px;
 `;
 
 const FormContainer = styled.form`
-min-width: 0%;
-display: flex;
-flex-direction: column;
-background: #ffffff;
-border-radius: 15px;
-margin-left: 20px;
-margin-right: 20px;
-padding-left: 65px;
-padding-right: 65px;
-padding-bottom: 70px;
-padding-top: 70px;
+  min-width: 0%;
+  display: flex;
+  flex-direction: column;
+  background: #ffffff;
+  border-radius: 15px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding-left: 65px;
+  padding-right: 65px;
+  padding-bottom: 70px;
+  padding-top: 70px;
 `;
 
 const NumberContainer = styled.div`
-display: flex;
-width: 40px;
-height: 40px;
-border-radius: 50%;
-background: #a9bcff;
-justify-content: center;
-align-items: center;
+  display: flex;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #a9bcff;
+  justify-content: center;
+  align-items: center;
 
-color: white;
-font-weight: 700;
+  color: white;
+  font-weight: 700;
 `;
 
 const FormTitle = styled.h4`
-color: #585858;
-font-weight: 700;
+  color: #585858;
+  font-weight: 700;
 `;
 
 const FormSubtitle = styled.h5`
-color: #7b7b7b;
-font-weight: 300;
+  color: #7b7b7b;
+  font-weight: 300;
 `;
 
 export default App;
